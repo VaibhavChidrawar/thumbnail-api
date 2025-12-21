235ba2e88bc0d23487a7b53bf42035744191029e
vaibhav chidrawar <87634517+VaibhavChidrawar@users.noreply.github.com>
2025-12-22 01:20:10 +0900

    Added helm charts and fixed images storage paths

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ REDIS_DB = os.getenv("REDIS_DB", "0")
 REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
 # Storage paths
-DATA_DIR = os.getenv("DATA_DIR", "/app/storage")
+DATA_DIR = os.getenv("STORAGE_PATH", "/app/storage")
 
 ORIGINALS_DIR = f"{DATA_DIR}/originals"
 THUMBNAILS_DIR = f"{DATA_DIR}/thumbnails"
